5af15bfc2c1c4cf48aa74b8f594707fce3390109
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-11-22 17:25:42 +0300

    Added transaction page

--- app/controllers/spendings_controller.rb
+++ app/controllers/spendings_controller.rb
@@ -1,4 +1,50 @@
 class SpendingsController < ApplicationController
-  def index; end
-  def show; end
+  before_action :set_group, only: %i[index new create]
+
+  def index
+    @group = Group.find(params[:group_id])
+    @spendings = @group.spendings.order(created_at: :desc)
+    @total = @spendings.map(&:amount).sum
+  end
+
+  def new
+    @spending = Spending.new
+    @group = Group.find(params[:group_id])
+  end
+
+  # def create
+  #   @spending = @group.spendings.build(spending_params)
+
+  #   if @spending.save
+  #     redirect_to group_spendings_path(@group), notice: 'Transaction added successfully.'
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  def create
+    @spending = Spending.new(spendings_params)
+    @group = Group.find(params[:group_id])
+
+    if @spending.save
+      SpendingGroup.create(group_id: @group.id, spending_id: @spending.id)
+      redirect_to group_spendings_path(@group), notice: 'Transaction was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def spending_params
+    params.require(:spending).permit(:name, :amount).merge(author_id: current_user.id)
+  end
+
+  def calculate_total(spendings)
+    spendings.sum(:amount)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get 'up' => 'rails/health#show', as: :rails_health_check
-  root "groups#index"
+  authenticated :user do
+    root 'groups#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'groups#index', as: :unauthenticated_root
+  end
+
+  devise_scope :user do
+    get 'users/sign_out', to: 'devise/sessions#destroy'
+  end
+
   resources :groups do
-    resources :spendings
+    resources :spendings, only: [:index, :new, :create]
   end
 end
