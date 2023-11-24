21d933cab1a5d038747903ebae95e3dcde4341db
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-11-25 02:42:17 +0300

    removed migration file

--- db/migrate/20231121040503_add_spending_ref_to_group_spendings.rb
+++
@@ -1,7 +0,0 @@
-class AddSpendingRefToGroupSpendings < ActiveRecord::Migration[7.1]
-  def change
-    ActiveRecord::Base.transaction do
-      add_reference :group_spendings, :spending, null: false, foreign_key: true
-    end
-  end
-end
