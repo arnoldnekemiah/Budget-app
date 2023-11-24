582d9287f99fcd2ba841fb35a00aa2d9a3ec45bd
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-11-25 02:34:23 +0300

    test

--- db/schema.rb
+++ db/schema.rb
@@ -65,8 +65,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_23_141939) do
     t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "group_id", null: false
     t.integer "author_id"
+    t.bigint "group_id", null: false
     t.bigint "user_id", null: false
     t.index ["group_id"], name: "index_spendings_on_group_id"
     t.index ["user_id"], name: "index_spendings_on_user_id"
