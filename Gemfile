f542f631a4588f5bcab019aa28518b4a364d5181
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-11-25 02:16:00 +0300

    removed safety measure t migigration file

--- Gemfile
+++ Gemfile
@@ -61,6 +61,7 @@ gem 'popper_js'
 
 gem 'sassc-rails'
 
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mswin mswin64 mingw x64_mingw]
--- db/migrate/20231121040503_add_spending_ref_to_group_spendings.rb
+++ db/migrate/20231121040503_add_spending_ref_to_group_spendings.rb
@@ -1,9 +1,7 @@
 class AddSpendingRefToGroupSpendings < ActiveRecord::Migration[7.1]
   def change
-    safety_assured do
-      ActiveRecord::Base.transaction do
-        add_reference :group_spendings, :spending, null: false, foreign_key: true
-      end
+    ActiveRecord::Base.transaction do
+      add_reference :group_spendings, :spending, null: false, foreign_key: true
     end
   end
 end
