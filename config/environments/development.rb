fc581dbe1440c1bd9b9654a1514f046a158ef9e8
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-11-25 04:31:10 +0300

    corrected host link3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ config.action_mailer.default_options = { from: 'no-reply@example.com' }
   config.action_mailer.smtp_settings = {
   address:         'smtp.gmail.com',
   port:            587,
-  domain:          'https://money-nm7c.onrender.com',
+  domain:          'gmail.com',
   user_name:       Rails.application.credentials.dig(:google_smtp, :email),
   password:        Rails.application.credentials.dig(:google_smtp, :password),
   authentication:  'plain',
@@ -48,7 +48,7 @@ config.action_mailer.default_options = { from: 'no-reply@example.com' }
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { host: 'money-nm7c.onrender.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'money-nm7c.onrender.com'}
 
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
